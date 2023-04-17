7b3513344645f8d76986e678677a01b2df91d885
halinad <gaxin@list.ru>
2023-04-17 16:01:29 +0300

    feat: complete simple-chain task

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -5,21 +5,24 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 const chainMaker = {
+  storage:[],
   getLength() {
     throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
   },
-  addLink(/* value */) {
+  addLink( value = "") {
     throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  removeLink( position ) {
+    this.storage = 0;
+    if (typeof position !== "number" || position <= 0 > this. getLength()) throw new Error("You can't remove incorrect link")
+    this.storage.splice(position - 1, 1);
+    return this;
   },
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    this.storage.reverse;
+    return this;
   },
   finishChain() {
     throw new NotImplementedError('Not implemented');
